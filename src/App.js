93145ba58cf5b8dc7265672cc3bf96e72c4bd111
tlanders <768713+tlanders@users.noreply.github.com>
2020-07-11 15:31:54 -0500

    added buttons to switch to different views

--- src/App.js
+++ src/App.js
@@ -32,13 +32,31 @@ class About extends React.Component {
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+        view: <Home/>
+    };
+    this.goToAbout = this.goToAbout.bind(this);
+    this.goToHome = this.goToHome.bind(this);
+  }
+
+    goToHome() {
+        this.setState({
+            view: <Home/>
+        });
+    }
+
+  goToAbout() {
+      this.setState({
+          view: <About/>
+      });
   }
 
   render() {
     return (
         <div className="App">
-          <Home/>
-          <About/>
+            {this.state.view}
+            <button onClick={this.goToAbout}>Go to About</button>
+            <button onClick={this.goToHome}>Go to Home</button>
         </div>
     );
   }
